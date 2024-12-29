5bf267c23b15438c6861cd2ab9f26ab54579bd95
Bilokha Danylo <danbilokha@gmail.com>
2024-12-29 19:01:17 +0100

    feat: added mui - ui lib

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export function Footer() {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/file.svg"
+          alt="File icon"
+          width={16}
+          height={16}
+        />
+        Terms and Conditions
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          aria-hidden
+          src="/window.svg"
+          alt="Window icon"
+          width={16}
+          height={16}
+        />
+        Cookies
+      </a>
+    </footer>
+  );
+}
